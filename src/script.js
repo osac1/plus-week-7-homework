96a6d972bbecad294fe9955679621e8cae41496f
osac1 <114300451+osac1@users.noreply.github.com>
2022-10-01 19:38:12 +0200

    Fixed html, started on JS

---
+++ src/script.js
@@ -0,0 +1,12 @@
+function citySearch(response){
+    event.preventDefault();
+
+}
+
+
+apiKey = "f81614abe2395d5dfecd45b9298041de"
+apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiUrl}`
+cityName = 
+
+newCity = document.querySelector("cityInput");
+newCity.addEventListener("submit", citySearch);
